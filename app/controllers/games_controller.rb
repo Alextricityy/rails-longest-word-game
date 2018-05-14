b242b3fdfdd3cb70c7a1ed456b3f68b3417414e8
Alex Sell <alextricityy@gmail.com>
2018-05-14 16:56:39 +0100

    fixed styling

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -18,19 +18,15 @@ class GamesController < ApplicationController
     if (include?(@letters, @attempt) == true)
       result = JSON.parse(open("https://wagon-dictionary.herokuapp.com/#{@attempt}").read)
       if(result["found"] == true)
-        @answer = "A word and in the grid"
+        @answer = "#{@attempt} is a word and in the grid"
         @score = ((@attempt.length)*3)
       else
-        @answer = "Not a word but in the grid"
+        @answer = "#{@attempt} is not a word but in the grid"
       end
     else
-      @answer = "Not in the grid and Not a word"
+      @answer = "#{@attempt} is Not in the grid and Not a word"
     end
   end
-
-
-
-
 
     def include?(letters, attempt)
       attempt.downcase.chars.all?{|letter| attempt.count(letter) <= letters.downcase.count(letter)}
